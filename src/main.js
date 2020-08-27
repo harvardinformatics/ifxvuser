3b7501aba52a302af5d0a12c467a0a0ab961252b
Aaron Kitzmiller <aaron.kitzmiller@gmail.com>
2020-08-27 09:41:59 -0400

    Maybe it's confused

--- src/main.js
+++ src/main.js
@@ -1,30 +1,30 @@
-import Vue from 'vue'
-import Vuetify from 'vuetify'
-import App from './App.vue'
+// import Vue from 'vue'
+// import Vuetify from 'vuetify'
+// import App from './App.vue'
 import { Request, AccountRequest, RequestAPI } from './request'
 import { UserAPI, User } from './user'
 import IFXUserList from './components/user/IFXUserList.vue'
 
-Vue.config.productionTip = false
-Vue.use(Vuetify, {
-  iconfont: 'md',
-  theme: {
-    primary: '#C62828',
-    secondary: '#90A4AE',
-    accent: '#5C6BC0',
-    error: '#db564c',
-    warning: '#fcf3a1',
-    info: '#2196f3',
-    success: '#4caf50'
-  },
-  options: {
-    customProperties: true
-  },
-})
+// Vue.config.productionTip = false
+// Vue.use(Vuetify, {
+//   iconfont: 'md',
+//   theme: {
+//     primary: '#C62828',
+//     secondary: '#90A4AE',
+//     accent: '#5C6BC0',
+//     error: '#db564c',
+//     warning: '#fcf3a1',
+//     info: '#2196f3',
+//     success: '#4caf50'
+//   },
+//   options: {
+//     customProperties: true
+//   },
+// })
 
-new Vue({
-  render: (h) => h(App),
-}).$mount('#app')
+// new Vue({
+//   render: (h) => h(App),
+// }).$mount('#app')
 
 function install(_Vue) {
   _Vue.component('IFXUserList', IFXUserList)
